36bf2ce214a77f7566d8eacb757cabe3464ad133
Lauren Thorud <lthorud12@gmail.com>
2025-03-19 23:25:55 -0500

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'cypress';
 import vitePreprocessor from 'cypress-vite';
-import { installPlugin } from '@chromatic-com/cypress';
+
 
 export default defineConfig({
   projectId: 'cprj7p',
@@ -19,6 +19,8 @@ export default defineConfig({
           console.log(message)
           return null;
         }
+
+      
       })
     },
   },
