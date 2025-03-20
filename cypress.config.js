33d8ca73cca0dc4d794353eb650ebaa7b63c59cc
Lauren Thorud <lthorud12@gmail.com>
2025-03-19 22:49:26 -0500

    update

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,16 @@ import vitePreprocessor from 'cypress-vite';
 import { installPlugin } from '@chromatic-com/cypress';
 
 export default defineConfig({
+  projectId: 'cprj7p',
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/results-[hash].xml',
+  },
   e2e: {
     baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      installPlugin(on, config);
+  
       on('file:preprocessor', vitePreprocessor());
     },
   },
